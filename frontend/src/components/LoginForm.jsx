352296a49fa0a6d9d3fa245c987c4fcf7b156506
karan7638 <karan2421kumar@gmail.com.com>
2025-08-06 11:03:13 +0530

    Initial commit

---
+++ frontend/src/components/LoginForm.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <form 
+        onSubmit={handleSubmit}
+        className="bg-white p-14 rounded-2xl shadow-xl w-full max-w-md"
+      >
+      <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">Login</h2>
+
+        <div className="mb-4">
+          <label htmlFor="username" className="block text-sm font-medium text-gray-700">
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+            className="mt-1 block w-full px-7 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+
+        <div className="mb-10">
+          <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition duration-200 font-semibold"
+        >
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import LoginForm from '../components/LoginForm';
+
+const Login = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-blue-100 to-purple-100">
+        <LoginForm />
+
+    </div>
+  );
+};
+
+export default Login;
